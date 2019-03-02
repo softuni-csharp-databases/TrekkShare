9fd4bb39c33fc5a5647cac20ffa221831030727e
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-03-02 16:40:04 +0200

    Added attributes.

--- TrekkShare/TrekkShare.Models/Route.cs
+++ TrekkShare/TrekkShare.Models/Route.cs
@@ -1,21 +1,32 @@
 ﻿namespace TrekkShare.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     using TrekkShare.Models.Enums.RouteEnums;
 
     public class Route
     {
+        [Key]
         public int RouteId { get; set; }
 
+        //[ForeignKey("Mountain)]
+        [Required]
         public int MountainId { get; set; }
 
+        [Required]
         public double Length { get; set; }
-
+        
+        [Required]
         public double Denivelation { get; set; }
 
+        [Required]
         public TerrainType TerrainType { get; set; }
 
+        [Required]
         public DifficultyLevel DifficultyLevel { get; set; }
 
+        // Това може да е отделен клас ? Някой добави таск в meistertask, но никой не го е поел.
+        [Required]
         public EquipmentLevel RecommendedEquipmentLevel { get; set; }
     }
 }
