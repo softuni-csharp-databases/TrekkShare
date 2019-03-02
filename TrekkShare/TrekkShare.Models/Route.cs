08669d8715264ccdac1183057d66027d10c838e4
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-03-02 16:02:22 +0200

    Added Route entity model.

---
+++ TrekkShare/TrekkShare.Models/Route.cs
@@ -0,0 +1,21 @@
+﻿namespace TrekkShare.Models
+{
+    using TrekkShare.Models.Enums.RouteEnums;
+
+    public class Route
+    {
+        public int RouteId { get; set; }
+
+        public int MountainId { get; set; }
+
+        public double Length { get; set; }
+
+        public double Denivelation { get; set; }
+
+        public TerrainType TerrainType { get; set; }
+
+        public DifficultyLevel DifficultyLevel { get; set; }
+
+        public EquipmentLevel RecommendedEquipmentLevel { get; set; }
+    }
+}
