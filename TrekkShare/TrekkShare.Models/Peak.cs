d23bd7001089ab81bd94712fd22bfc2cf8bc5ab9
YakoYakov <yako.p.yakov@gmail.com>
2019-03-02 18:07:06 +0200

    Added class Peak with comments for the navigation properties and difficulty level

---
+++ TrekkShare/TrekkShare.Models/Peak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TrekkShareClasses
+{
+    [Table("Peaks")]
+    public class Peak
+    {
+
+        [Key]
+        public int PeakId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The Peak Name cannot exceed 50 characters.")]
+        public string PeakName { get; set; }
+
+        public int PeakElevation { get; set; }
+
+        //public DifficultyLevel DifficultyLevel { get; set; }
+
+        //public int MountainId { get; set; }
+
+        //[ForeignKey(nameof(MountainId))]
+        //public Mountain Mountain { get; set; }
+
+        //public int CountryId { get; set; }
+
+        //[ForeignKey(nameof(CountryId))]
+        //public Country Country { get; set; }
+    }
+}
