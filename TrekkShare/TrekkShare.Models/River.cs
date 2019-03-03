2546faeecd4599c5d77d9846d69649c372ccc4e2
vallecbg <valioplayofficial@gmail.com>
2019-03-03 11:24:11 +0200

    created river model

--- TrekkShare/TrekkShare.Models/River.cs
+++ TrekkShare/TrekkShare.Models/River.cs
@@ -1,6 +1,34 @@
 ﻿namespace TrekkShare.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Routes")]
     public class River
     {
+        [Key]
+        public int RiverId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Length { get; set; }
+
+        public int Elevation { get; set; }
+
+        public int FlowStrength { get; set; }
+
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Discharge { get; set; }
+
+        [ForeignKey(nameof(MountainId))]
+        public int MountainId { get; set; }
+
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
     }
 }
