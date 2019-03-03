8aac92a24f0a35bcb5e4ec592dfc5e93ea7994a5
Aleksander Mitev <ajmitev@gmail.com>
2019-03-03 13:44:41 +0200

    Issue #7 and issue #8 fixed.

--- TrekkShare/TrekkShare.Models/Mountain.cs
+++ TrekkShare/TrekkShare.Models/Mountain.cs
@@ -2,7 +2,7 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Mountain
     {
         public Mountain()
