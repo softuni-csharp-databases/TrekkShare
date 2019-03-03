88e36e60c9f45eccccd92f6c5663df91fb95c0d5
Stoyan Shopov <stoyanshopov032@gmail.com>
2019-03-03 18:52:36 +0200

    Add DbSets for all models

--- TrekkShare/TrekkShare.Data/TrekkShareContext.cs
+++ TrekkShare/TrekkShare.Data/TrekkShareContext.cs
@@ -1,4 +1,6 @@
-﻿namespace TrekkShare.Data
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace TrekkShare.Data
 {
     using Microsoft.EntityFrameworkCore;
 
@@ -17,8 +19,20 @@
         {
         }
 
+        public DbSet<Cave> Caves { get; set; }
+        public DbSet<CaveCharacteristic> CaveCharacteristics { get; set; }
+        public DbSet<CaveDescription> CaveDescriptions { get; set; }
+        public DbSet<Cottage> Cottages { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Mountain> Mountains { get; set; }
+        public DbSet<Municipality> Municipalities { get; set; }
+        public DbSet<Peak> Peaks { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<River> Rivers { get; set; }
         public DbSet<Route> Routes { get; set; }
-
+        public DbSet<Tourist> Tourists { get; set; }
+        public DbSet<TouristTrip> TouristTrips { get; set; }
+        public DbSet<Town> Towns { get; set; }
         public DbSet<Trip> Trips { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
