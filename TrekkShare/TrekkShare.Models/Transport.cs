f2ef73f6f28f1ac4bbe5a7b82954f0c636266538
CryptoEcliptic <cryptoecliptic@gmail.com>
2019-03-03 15:09:36 +0200

    Added class Transport
    The class contains properties regarding Transport for a Trip such as: way of transport, time and place of departure, available places in the vehicle and approximate costs. depends on Enum TransportType

---
+++ TrekkShare/TrekkShare.Models/Transport.cs
@@ -0,0 +1,34 @@
+﻿namespace TrekkShare.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using TrekkShare.Models.Enums.TransportEnums;
+
+    public class Transport
+    { 
+        [Key]
+        public int TransportId { get; set; }
+
+        [Required]
+        public TransportType TransportType { get; set; }
+
+        [Required]
+        public DateTime DepartureTime { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        [Required]
+        public string DepartureAddress { get; set; }
+
+        public int Availableseats { get; set; }
+
+        [Required]
+        public decimal ExpectedCostPerPerson { get; set; }
+
+        public int TripId { get; set; }
+        public Trip Trip { get; set; }
+
+        public int TownId { get; set; }
+        public Town DepartureTown { get; set; }
+    }
+}
