c78e883c597811b4e80bb00448ad19fae9ce0109
Stoyan Shopov <stoyanshopov032@gmail.com>
2019-03-03 19:20:16 +0200

    add MountainConfig

---
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/MountainConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TrekkShare.Models;
+
+namespace TrekkShare.Data.EntityConfiguration
+{
+    public class MountainConfig : IEntityTypeConfiguration<Mountain>
+    {
+        public void Configure(EntityTypeBuilder<Mountain> builder)
+        {
+            builder.HasOne(x => x.HighestPeak)
+                .WithOne(x => x.Mountain)
+                .HasForeignKey<Mountain>(x => x.HighestPeakId);
+        }
+    }
+}
