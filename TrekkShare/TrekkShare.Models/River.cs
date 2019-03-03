d769d3e61d3e843c6378a4d72928816d352b7d99
vallecbg <valioplayofficial@gmail.com>
2019-03-03 12:05:19 +0200

    fixed errors

--- TrekkShare/TrekkShare.Models/River.cs
+++ TrekkShare/TrekkShare.Models/River.cs
@@ -1,9 +1,7 @@
 ﻿namespace TrekkShare.Models
 {
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-
-    [Table("Routes")]
+    
     public class River
     {
         [Key]
@@ -24,11 +22,9 @@
         public double Depth { get; set; }
 
         public double Discharge { get; set; }
-
-        [ForeignKey(nameof(MountainId))]
+        
         public int MountainId { get; set; }
-
-        [ForeignKey(nameof(CountryId))]
+        
         public int CountryId { get; set; }
 
         //OPTIONAL: kayak trips, boat trips
