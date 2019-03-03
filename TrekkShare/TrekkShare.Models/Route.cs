e91365f52079ab773367615edee13090c4a80c85
vallecbg <valioplayofficial@gmail.com>
2019-03-04 01:20:04 +0200

    Documented the optional part of the code and changed the name of the variables

--- TrekkShare/TrekkShare.Models/Route.cs
+++ TrekkShare/TrekkShare.Models/Route.cs
@@ -28,5 +28,40 @@
         // Това може да е отделен клас ? Някой добави таск в meistertask, но никой не го е поел.
         [Required]
         public EquipmentLevel RecommendedEquipmentLevel { get; set; }
+
+        public double StartLatitude { get; set; }
+
+        public double StartLongitude { get; set; }
+
+        public double EndLatitude { get; set; }
+
+        public double EndLongitude { get; set; }
+
+        //Optional: Calculate the distance
+        //public double Distance => CalculateDistanceInMeters(StartLatitude, StartLongitude,
+        //   EndLatitude, EndLongitude);
+
+        //private double CalculateDistanceInMeters (double lat1, double lon1,
+        //   double lat2, double lon2)
+        //{
+        //    var earthRadius = 6371e3;
+        //    var lat1Radians = ToRadians(lat1);
+        //    var lat2Radians = ToRadians(lat2);
+        //    var latResultRadians = ToRadians(lat2 - lat1);
+        //    var lonResultRadians = ToRadians(lon2 - lon1);
+
+        //    var halfChordLength = Math.Sin(latResultRadians / 2) * Math.Sin(latResultRadians / 2) +
+        //            Math.Cos(lat1Radians) * Math.Cos(lat2Radians) *
+        //            Math.Sin(lonResultRadians / 2) * Math.Sin(lonResultRadians / 2);
+        //    var angularDistanceRadius = 2 * Math.Atan2(Math.Sqrt(halfChordLength), Math.Sqrt(1 - halfChordLength));
+        //    var totalMeters = earthRadius * angularDistanceRadius;
+
+        //    return totalMeters;
+        //}
+
+        //private static double ToRadians(double val)
+        //{
+        //    return (Math.PI / 180) * val;
+        //}
     }
 }
