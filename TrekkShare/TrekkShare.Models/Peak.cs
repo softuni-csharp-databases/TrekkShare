158a73a10181ff309a47a5d2ce8c5a32de0e14bb
YakoYakov <yako.p.yakov@gmail.com>
2019-03-03 10:53:54 +0200

    Removed unused annotations, sort usings and removed unnecessary comments

--- TrekkShare/TrekkShare.Models/Peak.cs
+++ TrekkShare/TrekkShare.Models/Peak.cs
@@ -1,34 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using TrekkShare.Models.Enums.RouteEnums;
 
-namespace TrekkShareClasses
+namespace TrekkShare.Models
 {
-    [Table("Peaks")]
     public class Peak
     {
-
         [Key]
         public int PeakId { get; set; }
 
         [Required]
-        [StringLength(50, ErrorMessage = "The Peak Name cannot exceed 50 characters.")]
         public string PeakName { get; set; }
 
         public int PeakElevation { get; set; }
 
-        //public DifficultyLevel DifficultyLevel { get; set; }
+        public DifficultyLevel DifficultyLevel { get; set; }
 
-        //public int MountainId { get; set; }
+        public int MountainId { get; set; }
+        public Mountain Mountain { get; set; }
 
-        //[ForeignKey(nameof(MountainId))]
-        //public Mountain Mountain { get; set; }
-
-        //public int CountryId { get; set; }
-
-        //[ForeignKey(nameof(CountryId))]
-        //public Country Country { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
     }
 }
