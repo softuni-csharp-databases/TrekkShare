0d21e15de2a515babbac79a8a223bf2a3565f863
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2019-03-03 18:42:13 +0200

    Added Route entity config.

--- TrekkShare/TrekkShare.Data/EntityConfiguration/RouteConfig.cs
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/RouteConfig.cs
@@ -1,10 +1,18 @@
-﻿using System;
-namespace TrekkShare.Data.EntityConfiguration
+﻿namespace TrekkShare.Data.EntityConfiguration
 {
-    public class RouteConfig
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Models;
+
+    public class RouteConfig : IEntityTypeConfiguration<Route>
     {
-        public RouteConfig()
+
+        public void Configure(EntityTypeBuilder<Route> builder)
         {
+            builder.HasOne(x => x.Mountain)
+                .WithMany(x => x.Routes)
+                .HasForeignKey<Mountain>(x => x.MountainId);
         }
     }
 }
