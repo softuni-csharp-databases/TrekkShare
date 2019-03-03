f6fd42a723b5c0cbc0c25a86070d5cc9a3877249
Kiril Pecev <kir4o_pecev@abv.bg>
2019-03-03 22:53:38 +0200

    Modified Town class

--- TrekkShare/TrekkShare.Models/Town.cs
+++ TrekkShare/TrekkShare.Models/Town.cs
@@ -1,9 +1,15 @@
 ﻿namespace TrekkShare.Models
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
     public class Town
     {
+        public Town()
+        {
+            //this.Addresses = new HashSet<Addresses>();
+        }
+
         [Key]
         public int TownId { get; set; }
 
@@ -12,5 +18,7 @@
 
         public int MunicipalityId { get; set; }
         public Municipality Municipality { get; set; }
+
+        //public ICollection<Address> Addresses { get; set; }
     }
 }
