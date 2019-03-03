9fd732e5288f9b47f5245688145b01ea5a406d4b
RosenDev <33036545+RosenDev@users.noreply.github.com>
2019-03-03 10:13:19 +0200

    resolving #9

--- TrekkShare/TrekkShare.Models/CaveDescription.cs
+++ TrekkShare/TrekkShare.Models/CaveDescription.cs
@@ -1,23 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Security;
 
 namespace TrekkShare.Models
-{[Table("CavesDescriptions")]
+{
     public class CaveDescription
-    {[Key]
+    {
+        [Key]
         public int Id { get; set; }
-        [Required]
-        [Column(TypeName = "nvarchar(600)")]
-        [StringLength(600)]
-        public string Description { get; set; }
-        [Required]
-        [Column(TypeName = "nvarchar(600)")]
-        [StringLength(600)]
 
+        [Required]
+        [Column(TypeName = "nvarchar(600)")]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(600)")]
         public string History{ get; set;}
+
         [Column(TypeName = "nvarchar(450)")]
-        [StringLength(450)]
         public string Legend { get; set; }
 
     }
