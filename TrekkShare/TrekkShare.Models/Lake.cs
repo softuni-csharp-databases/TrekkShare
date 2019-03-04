e0bc25f33629d298f840874e49b722577996a67f
PanayotovPlamen <panaiotowplamen@gmail.com>
2019-03-04 22:11:36 +0200

    Added Lake Model

---
+++ TrekkShare/TrekkShare.Models/Lake.cs
@@ -0,0 +1,21 @@
+﻿namespace TrekkShare.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    class Lake
+    {
+        [Key]
+        public int LakeId { get; set; }
+     
+        [Required]
+        public string Name { get; set; }
+
+        public double Surface { get; set; }
+
+        public double Depth { get; set; }
+
+        public int MountainId { get; set; }
+        public Mountain Mountain { get; set; }
+   
+    }
+}
