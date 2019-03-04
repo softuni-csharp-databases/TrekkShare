076880d19639e5b63f49a2c51678a53d144f310a
nelly Ifandieva <n.ifandieva@gmail.com>
2019-03-04 15:31:40 +0200

    Add files via upload
    This is a version of the Address class, not yet completed

---
+++ TrekkShare/TrekkShare.Models/Address.cs
@@ -0,0 +1,34 @@
+﻿namespace TrekkShare.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using TrekkShare.Services.Contracts;
+
+    public class Address : IAddress
+    {
+        private int id;
+        private string text;
+        private int townId;
+        private Town town;
+
+        public Address(string text)
+        {
+            //this.Id = AssignAddressId();
+            //this.Text = text;
+            //this.TownId = Town.AssignTownId();
+            //Town town = 
+        }
+
+        [Key]
+        public int Id { get;  }
+        public string Text { get; }
+        [ForeignKey(Town(Id))]
+        public int TownId { get; }
+        public Town Town { get; }
+
+        private static void AssignAddressId()
+        {
+            //this.Id++;
+        }
+    }
+}
