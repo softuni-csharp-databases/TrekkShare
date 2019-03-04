f87cd2840df2d7777b5de1865560b1155a583c33
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2019-03-04 12:07:56 +0200

    Changed relatioship from Town to Mountain. Removed unecessary namespaces and did some refactoring.

--- TrekkShare/TrekkShare.Models/Cave.cs
+++ TrekkShare/TrekkShare.Models/Cave.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace TrekkShare.Models
 {
     public class Cave
-    
+
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
-        
+
         public decimal TicketPrice { get; set; }
 
         public int StartingTime { get; set; }
@@ -19,10 +14,9 @@ namespace TrekkShare.Models
         public int EndingTime { get; set; }
 
         public int TimeIntervalWithGuide { get; set; }
-       
-        public int TownId { get; set; }
 
-         public Town Town { get; set; }
+        public int MountainId { get; set; }
+        public Mountain Mountain { get; set; }
 
     }
 }
