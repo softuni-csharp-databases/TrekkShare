3e58e305f0b678b0cecccfee60765d2b2ab89a4c
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2019-03-04 12:31:26 +0200

    Refactored Mountain config and removed unecessary relatioship to peaks.

--- TrekkShare/TrekkShare.Data/EntityConfiguration/MountainConfig.cs
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/MountainConfig.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using TrekkShare.Models;
-
 namespace TrekkShare.Data.EntityConfiguration
 {
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using TrekkShare.Models;
+
     public class MountainConfig : IEntityTypeConfiguration<Mountain>
     {
         public void Configure(EntityTypeBuilder<Mountain> builder)
         {
-            builder.HasOne(x => x.HighestPeak)
-                .WithOne(x => x.HighestPeakMountain)
-                .HasForeignKey<Mountain>(x => x.HighestPeakId)
-                .OnDelete(DeleteBehavior.Restrict);
+            // TODO
         }
     }
 }
