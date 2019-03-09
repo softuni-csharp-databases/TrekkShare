7b6a005961544b727566a79f90399bc5db731f5e
NaskoVasilev <nasko_vasilev@abv.bg>
2019-03-09 22:00:19 +0200

    Add address and cottage configuration

---
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/AddressConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace TrekkShare.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Models;
+
+    public class AddressConfig : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(a => a.AddressId);
+
+            builder.HasOne(a => a.Town)
+                .WithMany(t => t.Addresses)
+                .HasForeignKey(a => a.TownId);
+
+            builder.HasMany(a => a.Users)
+                .WithOne(u => u.Address)
+                .HasForeignKey(u => u.AddressId);
+        }
+    }
+}
---
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/CottageConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace TrekkShare.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TrekkShare.Models;
+
+    public class CottageConfig : IEntityTypeConfiguration<Cottage>
+    {
+        public void Configure(EntityTypeBuilder<Cottage> builder)
+        {
+            builder.HasKey(c => c.CottageId);
+
+            builder.Property(c => c.Name)
+                .IsUnicode();
+
+            builder.HasOne(c => c.Mountain)
+                .WithMany(m => m.Cottages)
+                .HasForeignKey(c => c.MountainId);
+
+            //TODO: Add relation to GeoLocation
+        }
+    }
+}
