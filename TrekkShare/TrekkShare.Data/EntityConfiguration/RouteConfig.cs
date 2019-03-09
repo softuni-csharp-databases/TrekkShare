1dd0d1e392a022754ea6031feafd588145711b1d
SonnyRR <9307969+SonnyRR@users.noreply.github.com>
2019-03-09 22:38:11 +0200

    Updated RouteConfig.

--- TrekkShare/TrekkShare.Data/EntityConfiguration/RouteConfig.cs
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/RouteConfig.cs
@@ -10,9 +10,18 @@ namespace TrekkShare.Data.EntityConfiguration
 
         public void Configure(EntityTypeBuilder<Route> builder)
         {
-            builder.HasOne(x => x.Mountain)
-                .WithMany(x => x.Routes)
-                .HasForeignKey(x => x.MountainId);
+            builder.HasKey(x => x.RouteId);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(30)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(x => x.StartGeoPoint)
+                .IsRequired(true);
+
+            builder.Property(x => x.EndGeoPoint)
+                .IsRequired(true);
         }
     }
 }
