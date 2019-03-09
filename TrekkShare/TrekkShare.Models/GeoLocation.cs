b65268e7b992a1ac3b953bfd36dbfb64a3b4076c
Stoyan Shopov <stoyanshopov032@gmail.com>
2019-03-09 14:07:23 +0000

    introduce GeoLocation entity

---
+++ TrekkShare/TrekkShare.Models/GeoLocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NetTopologySuite.Geometries;
+
+namespace TrekkShare.Models
+{
+    public class GeoLocation
+    {
+        public int Id { get; set; }
+
+        public Point Location { get; set; }
+
+        public string Address { get; set; }
+    }
+}
