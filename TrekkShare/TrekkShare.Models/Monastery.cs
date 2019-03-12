f80b00f12a670e726bc426521e5304d4b59739ec
RosenDev <ros20021999@gmail.com>
2019-03-12 22:16:49 +0200

    Added Monastery Model

---
+++ TrekkShare/TrekkShare.Models/Monastery.cs
@@ -0,0 +1,13 @@
+﻿namespace TrekkShare.Models
+{
+    public class Monastery
+    {
+        public int MonasteryId { get; set; }
+
+        public string MonasteryName { get; set; }
+
+        public int MountainId { get; set; }
+        public Mountain Mountain { get; set; }
+
+    }
+}
