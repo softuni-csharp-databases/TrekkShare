7c7196c1dbc67e82e49bce4f40b4fa8e5b2fd92c
NickolayNanov <40512673+NickolayNanov@users.noreply.github.com>
2019-03-13 22:16:40 +0200

    ProvinceConfig Added

--- TrekkShare/TrekkShare.Models/Province.cs
+++ TrekkShare/TrekkShare.Models/Province.cs
@@ -1,8 +1,6 @@
 ﻿namespace TrekkShare.Models
 {
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Province
     {
@@ -11,14 +9,10 @@
             this.Municipalities = new HashSet<Municipality>();
         }
 
-        [Key]
         public int ProvinceId { get; set; }
 
-        [Required]
-        [Column(TypeName = "nvarchar(75)")]
         public string Name { get; set; }
 
-        [Required]
         public int CountryId { get; set; }
         public Country Country { get; set; }
 
