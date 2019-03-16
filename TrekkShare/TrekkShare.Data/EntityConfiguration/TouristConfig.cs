0089c8fab22e871cab6fdf3df297065da84b4431
NaskoVasilev <nasko_vasilev@abv.bg>
2019-03-16 22:47:18 +0200

    Add Tourist Config

---
+++ TrekkShare/TrekkShare.Data/EntityConfiguration/TouristConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace TrekkShare.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using TrekkShare.Models;
+
+    public class TouristConfig : IEntityTypeConfiguration<Tourist>
+    {
+        public void Configure(EntityTypeBuilder<Tourist> builder)
+        {
+            builder.HasKey(t => t.TouristId);
+
+            builder.HasOne(t => t.User)
+                .WithOne(u => u.Tourist);
+
+            builder.HasMany(t => t.TouristTrips)
+                .WithOne(tr => tr.Tourist)
+                .HasForeignKey(tr => tr.TouristId);
+        }
+    }
+}
