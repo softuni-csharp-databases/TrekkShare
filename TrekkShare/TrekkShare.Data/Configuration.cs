eec9dde7dca98f418580f15a7261d93a5b4aca72
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-03-21 13:42:09 +0200

    Removed wrongly uploaded config file.

--- TrekkShare/TrekkShare.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-namespace ConnectionProject
-{
-
-    public static class Configuration
-    {
-        public const string ConnectionString = @"Server=localhost,1433;Database=TrekkShare;User Id=sa;Password=EmanuelaTop1.;";
-    }
-}
